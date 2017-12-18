77f888f1b75af5dae1cf822328c56f1538c5e6f4
Xavier Martínez <xmartinezpujol@gmail.com>
2017-12-18 18:12:44 +0100

    Initial config

---
+++ src/Player.js
@@ -0,0 +1,7 @@
+class Player {
+  constructor(name) {
+
+  }
+}
+
+export default Player;
---
+++ src/player.test.js
@@ -0,0 +1,7 @@
+import Player from './Player';
+
+describe('Player', () => {
+  it('works', () => {
+
+  });
+});
